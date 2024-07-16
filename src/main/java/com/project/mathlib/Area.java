85ccd4cc04c9046030e87794af27919f996e49ee
talecood <goktug.tabakk@gmail.com>
2024-07-16 16:03:42 +0300

    16.07.2024

--- src/main/java/com/project/mathlib/Area.java
+++ src/main/java/com/project/mathlib/Area.java
@@ -1,14 +1,56 @@
 package com.project.mathlib;
 
-import com.project.*;
+import com.project.mathlib.Base;
+import com.project.mathlib.Power;
+
 
 public class Area {
-	private Area() {
+	public Area() {
+	}
+	public static final double PI= 3.141592653589793;
+	public static final String POSITIVE_RADIUS = "Value must be a positive radius.";
+	public static final String POSITIVE_HEIGHT = "Value must be a positive height.";
+	public static final String POSITIVE_BASE = "Value must be a positive base.";
+	
+	
+	public int squareArea(int a) {
+		if(a<0) {
+			throw new IllegalArgumentException(POSITIVE_BASE);
+		}
+		return (int) Math.pow(a,2);
+	}
+	public int rectangleArea(int a,int b) {
+		if(a<0 || b<0) {
+			throw new IllegalArgumentException(POSITIVE_BASE+POSITIVE_HEIGHT);
+		}
+		return a*b;
 	}
 	
-	private static final String POSITIVE_RADIUS = "Value must be a positive radius.";
-	private static final String POSITIVE_HEIGHT = "Value must be a positive height.";
-	private static final String POSITIVE_BASE = "Value must be a positive base.";
+	public double circleArea(double r) {
+		if(r<0) {
+			throw new IllegalArgumentException(POSITIVE_RADIUS);
+		}
+		return(Math.pow(r, 2)*PI);
+	}
+	public double triangleArea(double height,double base) {
+		if(height<0||base<0) {
+			throw new IllegalArgumentException(POSITIVE_BASE+POSITIVE_HEIGHT);
+		}
+		return (base*height)/2;
+	}
+	public double parallelogramArea(double base,double height) {
+		if(base<0||height<0) {
+			throw new IllegalArgumentException(POSITIVE_BASE+POSITIVE_HEIGHT);
+		}
+		return base*height;
+	}
+	public double trapeziumArea(double upperBase,double lowerBase,double height) {
+		if(upperBase<0 || lowerBase<0 || height<0) {
+			throw new IllegalArgumentException(POSITIVE_BASE+POSITIVE_HEIGHT);
+		}
+		return ((lowerBase+upperBase)*height)/2;
+	}
 	
+
 	
 }
---
+++ src/test/java/com/project/test/AreaTest.java
@@ -0,0 +1,144 @@
+package com.project.test;
+
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.fail;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.api.TestReporter;
+
+import com.project.mathlib.Area;
+
+public class AreaTest {
+	Area objectArea= new Area();
+	
+	TestInfo testinfo;
+	TestReporter testreporter;
+	
+	@BeforeEach
+	void setUp(TestInfo testinfo,TestReporter testreporter) {
+	objectArea = new Area();
+	this.testinfo = testinfo;
+	this.testreporter = testreporter;
+	}
+	@AfterEach
+	void afterEach() {
+		  testreporter.publishEntry("Running " + testinfo.getDisplayName() + " with tags " + testinfo.getTags());
+	}
+	
+	@Nested
+	@DisplayName("Area Test for Objects")
+	class AreaTests{
+		@Tag("Basic Objects")
+		@Test
+		@DisplayName("Square Area Test")
+		void ShouldReturnAreaOfSquare() {
+			assertAll(
+					()-> assertEquals(1,objectArea.squareArea(1),"Square Test Failed"),
+					()-> assertEquals(0,objectArea.squareArea(0),"Square Test Failed"),
+					()-> assertEquals(100,objectArea.squareArea(10),"Square Test Failed")
+					);
+		try {
+			assertEquals(100,objectArea.squareArea(-10));
+			fail("Should Have thrown IllegalArgumentException");
+		}	catch(Exception e) {
+			assertEquals(objectArea.POSITIVE_BASE,e.getMessage());
+		}
+	}
+		
+	@Tag("Basic Objects")
+	@Test
+	@DisplayName("Rectangle Area Test")
+	void ShouldReturnAreaOfRectangle() {
+			assertAll(
+					() -> assertEquals(0,objectArea.rectangleArea(0, 5),"Rectangle Test Failed"),
+					() -> assertEquals(10,objectArea.rectangleArea(2, 5),"Rectangle Test Failed")
+					);
+			try {
+				assertEquals(-5,objectArea.rectangleArea(-5, 1));
+				assertEquals(10,objectArea.rectangleArea(-2, -5));
+				fail("Should have thrown IllegalArgumentException");
+			}
+			catch(Exception e) {
+				assertEquals(objectArea.POSITIVE_BASE+objectArea.POSITIVE_HEIGHT,e.getMessage());
+			}
+	}
+	@Tag("Basic Objects")
+	@Test
+	@DisplayName("Circle Area Test")
+	void ShouldReturnAreaOfCircle() {
+		assertAll(
+				() -> assertEquals(50.26548245743669,objectArea.circleArea(4)),
+				() -> assertEquals(0,objectArea.circleArea(0))
+				);
+		
+		try {
+			assertEquals(78,5398,objectArea.circleArea(-5));
+		}
+		catch(Exception e) {
+			assertEquals(objectArea.POSITIVE_RADIUS,e.getMessage());
+		}
+		
+	}
+	@Tag("Basic Objects")
+	@Test
+	@DisplayName("Triangle Area Test")
+	void ShouldReturnAreaOfTriangle() {
+		assertAll(
+				() -> assertEquals(12.0,objectArea.triangleArea(3.0, 8.0)),
+				() -> assertEquals(0.0,objectArea.triangleArea(0.0, 3.0))
+				);
+		try {
+			assertEquals(-100,objectArea.triangleArea(-50.0, 4.0),"Triangle Test Failed");
+			assertEquals(-100,objectArea.triangleArea(50.0, -4.0),"Triangle Test Failed");
+			assertEquals(100,objectArea.triangleArea(-50.0, -4.0),"Triangle Test Failed");
+		}
+		catch(Exception e) {
+			assertEquals(objectArea.POSITIVE_BASE+objectArea.POSITIVE_HEIGHT,e.getMessage());
+		}
+	}
+	@Tag("Basic Objects")
+    @Test
+    @DisplayName("Parallelogram Area Test")
+    void shouldReturnAreaOfParallelogram() {
+        assertAll(
+                () -> assertEquals(20.0, objectArea.parallelogramArea(5.0, 4.0)),
+                () -> assertEquals(0.0, objectArea.parallelogramArea(0.0, 4.0))
+        );
+        try {
+			assertEquals(-100,objectArea.parallelogramArea(-50.0, 2.0),"Triangle Test Failed");
+			assertEquals(-100,objectArea.parallelogramArea(50.0, -2.0),"Triangle Test Failed");
+			assertEquals(100,objectArea.parallelogramArea(-50.0, -2.0),"Triangle Test Failed");
+		}
+		catch(Exception e) {
+			assertEquals(objectArea.POSITIVE_BASE+objectArea.POSITIVE_HEIGHT,e.getMessage());
+		}
+	}
+	@Tag("Basic Objects")
+	@Test
+	@DisplayName("Trapezium Area Test")
+	void shouldReturnAreaOfTrapezium() {
+		assertAll(
+				() ->	assertEquals(16.0,objectArea.trapeziumArea(3,5,4)),
+				() ->	assertEquals(0,objectArea.trapeziumArea(6, 2, 0))
+				);
+		try {
+			assertEquals(16.0,objectArea.trapeziumArea(-5, 13, 4));
+			assertEquals(16.0,objectArea.trapeziumArea(29, -13, 4));
+			assertEquals(16.0,objectArea.trapeziumArea(-5, -3, -4));
+		}
+		catch(Exception e) {
+			assertEquals(objectArea.POSITIVE_BASE+objectArea.POSITIVE_HEIGHT,e.getMessage());
+		}
+	}
+	
+		
+		
+	}
+}
